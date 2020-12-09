c7e514ca35c8fc8ef746340080e621b7c67a766a
Zaur K. Hasanov <Zaur.Hasanov@kapitalbank.az>
2020-12-09 12:24:14 +0400

    crated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
