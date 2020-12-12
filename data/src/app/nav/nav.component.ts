6ad661de2403704738317ac44fbb735981994263
Zaur K. Hasanov <Zaur.Hasanov@kapitalbank.az>
2020-12-12 23:17:21 +0400

    categories taken from remote

--- data/src/app/nav/nav.component.ts
+++ data/src/app/nav/nav.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+class Category {
+  id: number | undefined;
+  name: string | undefined;
+}
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +14,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  categories: any;
+
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit(): void {
+    this.http.get<Category[]>('https://sell-social-dev.herokuapp.com/api/v1/categories')
+      .subscribe((data: Category[]) => {
+        this.categories = data;
+      }, (err) => {
+        console.log(err);
+      });
   }
 
 }
+
